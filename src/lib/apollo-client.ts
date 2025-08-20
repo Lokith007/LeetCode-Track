cd872821024a4dbd552a0462f6795292c7265507
MuthuTej <muthu1410raj@gmail.com>
2025-08-20 13:02:42 +0530

    url change

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -3,7 +3,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://backend-leetcode.onrender.com',
+  uri: 'https://backend-leetcode.onrender.com/graphql',
   cache: new InMemoryCache(),
 });
 
